1442a814d77efeb00979dc5305e27ee8c155e031
Alejandro Escanero Blanco <aescanero@gmail.com>
2023-06-06 19:41:30 +0200

    update

--- 05/activar_acceso_kubernetes.sh
+++ 05/activar_acceso_kubernetes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-USER=$(kubectl oidc_login setup --oidc-issuer-irl=https://manager.workshop:5554/dex --oidc-client-id=kubernetes --oidc-client-secret=aGVsbG9fZnJpZW5kCg== 2>&1|grep sub|cut -d\" -f 3)
-kubectl create clusterrolebinding oidc-cluster-admin --clusterrole=cluster-admin --user='https://manager.workshop:5554/dex#'${USER}
+USER=$(kubectl oidc_login setup --oidc-issuer-url=https://manager.workshop:5554/dex --oidc-client-id=kubernetes --oidc-client-secret=aGVsbG9fZnJpZW5kCg== 2>&1|grep '"sub"'|cut -d\" -f 4)
+sudo kubectl create clusterrolebinding oidc-cluster-admin --clusterrole=cluster-admin --user='https://manager.workshop:5554/dex#'${USER}
 kubectl config set-credentials oidc \
 	  --exec-api-version=client.authentication.k8s.io/v1beta1 \
 	  --exec-command=kubectl \
